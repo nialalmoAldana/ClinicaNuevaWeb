d9711034dfb73aa974a0bb1af8f768feff6afc99
Nicolas Alejandro Aldana Moreno <naaldanam@clinica.local>
2023-03-10 11:32:08 -0500

    0.3

--- js/script.js
+++ js/script.js
@@ -33,3 +33,80 @@ window.addEventListener('resize', () => {
         document.body.classList.remove('resize-animation-stopper');
     }, 400);
 });
+
+const tabLinks = document.querySelectorAll(".tab-link");
+const tabContents = document.querySelectorAll(".tab-content");
+
+tabLinks.forEach(function (tabLink) {
+  tabLink.addEventListener("click", function () {
+    const tabId = this.getAttribute("data-tab");
+    const activeTab = document.querySelector(`#${tabId}`);
+    tabLinks.forEach(function (tabLink) {
+      tabLink.classList.remove("active");
+    });
+    tabContents.forEach(function (tabContent) {
+      tabContent.classList.remove("active");
+    });
+    this.classList.add("active");
+    activeTab.classList.add("active");
+  });
+});
+
+
+
+const slider = document.querySelector('#slider');
+const sliderContainer = document.querySelector('.slider-container');
+const sliderImages = document.querySelectorAll('.slider-image');
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+
+let currentIndex = 0;
+let intervalId;
+
+function initSlider() {
+  sliderImages[currentIndex].classList.add('active');
+  startSlider();
+}
+
+function startSlider() {
+  intervalId = setInterval(() => {
+    resetSlider();
+    if (currentIndex === sliderImages.length - 1) {
+      currentIndex = 0;
+    } else {
+      currentIndex++;
+    }
+    sliderImages[currentIndex].classList.add('active');
+  }, 3000);
+}
+
+function resetSlider() {
+  sliderImages.forEach(image => {
+    image.classList.remove('active');
+  });
+}
+
+function prevSlide() {
+  resetSlider();
+  if (currentIndex === 0) {
+    currentIndex = sliderImages.length - 1;
+  } else {
+    currentIndex--;
+  }
+  sliderImages[currentIndex].classList.add('active');
+}
+
+function nextSlide() {
+  resetSlider();
+  if (currentIndex === sliderImages.length - 1) {
+    currentIndex = 0;
+  } else {
+    currentIndex++;
+  }
+  sliderImages[currentIndex].classList.add('active');
+}
+
+prevBtn.addEventListener('click', prevSlide);
+nextBtn.addEventListener('click', nextSlide);
+
+initSlider();
